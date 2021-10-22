8e82db43a39d899eddef69b2000116f5d1b548c0
Kaushik Thumar <92664692+KaushikThumar@users.noreply.github.com>
2021-10-22 22:44:25 +0530

    Add files via upload

---
+++ SERVO.ino
@@ -0,0 +1,21 @@
+//SERVO CONNECTIONS --ORANGE-SIGNAL,RED-5V,BROWN-GND
+
+
+#include <Servo.h> 
+ Servo myservo;  // create servo object to control a servo 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  
+} 
+void loop() 
+{ 
+myservo.write(0); // sets the servo position according to the scaled value   
+  delay(1000);     
+    myservo.write(90); //90 DEGREE  
+  delay(1000);   
+  myservo.write(180);   
+  delay(1000);   
+
+  
+} 
